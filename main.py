0c9d4da2d086fd8ce4d0dfdee52e6d4b01d1b16f
Sang-heon <choi.sg@obmontessori.edu.ph>
2026-01-19 09:55:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Temperature Converter: Fahrenheit to Celsius
+from pyscript import display, document # pyright: ignore[reportMissingImports]
+
+def temperature_conversion(e):
+    document.getElementById('output').innerHTML = ""
+
+    fahrenheit = float(document.getElementById('temperature').value)
+    
+    celsius = (fahrenheit - 32) * 5/9
+
+    temperature = round(celsius, 1)
+
+    if temperature < 37.8:
+        display(f'Your temperature is: {temperature} °C, you are healthy.', target='output')
+    elif temperature > 37.8:
+        display(f'Your temperature is: {temperature} °C, you have a fever.', target='output')
+    else:
+        display(f'Invalid input', target='output')
