c71066aebf437d09b3b10f5c3b96713510604b9a
Sang-heon <choi.sg@obmontessori.edu.ph>
2026-01-21 09:58:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def temperature_conversion(e):
     temperature = round(celsius, 1)
 
     if temperature <= 37.8:
-        display(f'Your temperature is: {temperature} °C, you are healthy.', target='output')
+        display(f'Your temperature is: {temperature} °C, you don't have a fever.', target='output')
     elif temperature > 37.8:
         display(f'Your temperature is: {temperature} °C, you have a fever.', target='output')
     else:
         display(f'Invalid input', target='output')
+
 
